61e70db2bd578c9848f129e75e93bb431222d46a
zolfaShefreie <z.shefreie@gmail.com>
2021-06-29 11:42:53 +0430

    fix the bug of project name

--- BookEcho/wsgi.py
+++ BookEcho/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BookEcho_1 project.
+WSGI config for BookEcho project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookEcho_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookEcho.settings')
 
 application = get_wsgi_application()
