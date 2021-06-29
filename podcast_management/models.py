d309503f632b56993eba74c1290a786dc409cb59
zolfaShefreie <z.shefreie@gmail.com>
2021-06-29 11:34:46 +0430

    add podcast model

--- podcast_management/models.py
+++ podcast_management/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Podcast(models.Model):
+    file = models.FileField(upload_to='podcast', null=True)
+    request = models.OneToOneField("request_management.", on_delete=models.CASCADE)
+    delivery_date = models.DateField(null=True)
+    description = models.TextField(blank=True)
+    is_private = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+
