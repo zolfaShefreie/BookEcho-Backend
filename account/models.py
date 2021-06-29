6ec049cb2fff41e4e3ff411daeed911105716085
zolfaShefreie <z.shefreie@gmail.com>
2021-06-29 11:34:02 +0430

    complete model of account

--- account/models.py
+++ account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+from utils.utils import Enum
+
+
+class Choices:
+
+    class UserType(Enum):
+        podcast_producer = 'p'
+        normal = 'n'
+
+
+class User(AbstractUser):
+    email = models.EmailField(null=False)
+    user_type = models.CharField(max_length=True, choices=Choices.UserType.choices(), null=False)
+    avatar = models.ImageField(upload_to='avatar', null=True)
+    USERNAME_FIELD = "email"
+
+
+class ProducerInfo(models.Model):
+    voice_sample = models.FileField(upload_to="voice_sample")
+    score = models.PositiveIntegerField(default=0)
+    podcast_producer = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
