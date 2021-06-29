a27ba41e08336d3dd08f586fe3e73f3523210a3b
zolfaShefreie <z.shefreie@gmail.com>
2021-06-29 11:35:12 +0430

    add request model

--- request_management/models.py
+++ request_management/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from utils.utils import Enum
+
+
+class Choices:
+
+    class RequestStatus(Enum):
+        accepted = 'ac'
+        rejected = 'r'
+        pending = 'p'
+        active = 'a'
+        inactive = 'i'
+
+
+class Request(models.Model):
+    file = models.FileField(upload_to='pdf')
+    applicant = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    podcast_producer = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=Choices.RequestStatus.choices(), default='p')
+    deadline = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
