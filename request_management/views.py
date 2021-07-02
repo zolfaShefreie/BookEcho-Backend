4c71bafcb67956475b2c3c70221e6da3623ff94b
zolfaShefreie <z.shefreie@gmail.com>
2021-07-02 14:47:46 +0430

    add request create view

--- request_management/views.py
+++ request_management/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from django.db import transaction
 
-# Create your views here.
+from . import serializers
+from . import models
+from . import permissions
+from utils.utils import CustomPageNumberPage
+from utils.views import RelatedObjCreateView
+from account.models import User
+from account import permissions as account_permissions
+
+
+class RequestCreateView(RelatedObjCreateView):
+    related_obj_name = 'podcast_producer'
+    queryset = User.objects.filter(user_type='p').exclude(info=None)
+    permission_classes = (IsAuthenticated, permissions.ProducerNotReqUserPermission,
+                          account_permissions.CompleteRegisterPermission)
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'pk'
+
+
+
